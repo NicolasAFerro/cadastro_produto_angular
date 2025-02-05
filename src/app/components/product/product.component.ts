db401b0756a2f1dcd0f65702baf3ab6f0bacf9ee
Nicolas Ferro <nicolasalexandrinoferro@gmail.com>
2025-02-05 12:37:27 -0300

    diretiva ngIf

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -10,15 +10,17 @@ import { Product } from '../../interfaces/Product';
   styleUrl: './product.component.css',
 })
 export class ProductComponent {
-  product: Product= {} as Product;
+  @Input()
+  product ?: Product;
+
   @Input()
   sonCategories: Category[] = [];
 
   teste(){
-    console.log(`nome: ${this.product.name}\n descrição: ${this.product.description}`);
-    console.log(`\n preço: ${this.product.price}\n promoção: ${this.product.promotion}`);
-    console.log(`\n new: ${this.product.newProduct}\n categoria: ${this.product.category.name}`);
-    console.log(`\n ${this.product.category.id} `);
+    // console.log(`nome: ${this.product.name}\n descrição: ${this.product.description}`);
+    // console.log(`\n preço: ${this.product.price}\n promoção: ${this.product.promotion}`);
+    // console.log(`\n new: ${this.product.newProduct}\n categoria: ${this.product.category.name}`);
+    // console.log(`\n ${this.product.category.id} `);
 
   }
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Category } from '../../interfaces/category';
+import { Product } from '../../interfaces/Product';
 
 @Component({
   selector: 'app-products',
@@ -9,6 +10,12 @@ import { Category } from '../../interfaces/category';
   styleUrl: './products.component.css',
 })
 export class ProductsComponent {
+  product: Product={}as Product;
+  //se eud deixar assim, eu consigo condicionar oq quero ou não mostrar o componente.
+  //ai todo o templante/layout do component filho desapareça
+  //product ?: Product;
+
+
   categories: Category[] = [
     {
       id: 1,
